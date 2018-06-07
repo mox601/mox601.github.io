5f3a3e7dd2e68f2ce2b715620400a53b227b1e45
Matteo Moci <mox601@gmail.com>
2018-06-07 23:51:19 +0200

    one post

---
+++ path---index-a154ab298f8a2c72305d.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{395:function(e,t){e.exports={data:{site:{siteMetadata:{title:"Gatsby Starter Blog"}},allMarkdownRemark:{edges:[{node:{excerpt:"This is my first post on my new fake blog! How exciting! I’m sure I’ll write a lot more interesting things in the future. Oh, and here’s a…",fields:{slug:"/hello-world/"},frontmatter:{date:"01 May, 2015",title:"Hello World"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---index-a154ab298f8a2c72305d.js.map
